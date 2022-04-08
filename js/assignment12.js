b4f35124bfc19f540923772a3c068cc09a76be9e
nabu1980 <nabu1980@colorado.edu>
2022-04-08 09:54:38 -0600

    assignment 12 finish

--- js/assignment12.js
+++ js/assignment12.js
@@ -8,11 +8,15 @@
 
 const jokeButton = document.querySelector('#jsNewJoke');
 const jokeHolder = document.querySelector('#jsJokeText');
+const likeButton = document.querySelector('#jsLike');
+
 jokeButton.addEventListener('click', newJoke);
+likeButton.addEventListener('click', liked);
+
 const endPoint = 'https://icanhazdadjoke.com';
 
 async function newJoke() {
-   console.log("joke button was clicked");
+   // console.log("joke button was clicked");
   try {
     const response = await fetch(endPoint,
       {headers: {
@@ -23,7 +27,7 @@ async function newJoke() {
       throw Error(response.statusText);
     }
     const joke = await response.json();
-    console.log(joke);
+    // console.log(joke);
     displayJoke(joke.joke);
   } catch(err) {
     console.log(err);
@@ -34,4 +38,10 @@ async function newJoke() {
 function displayJoke(text) {
   const jokeText = document.querySelector('#jsJokeText');
   jokeText.textContent = text;
+}
+
+async function liked() {
+  // console.log("like button was clicked");
+  const likeText = document.querySelector('#jsJokeText');
+  likeText.textContent = "Really? You liked this one?";
 }
